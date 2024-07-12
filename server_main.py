d8badaebb3729bec6346dbd025ac621366afb364
lin <lin0102x@163.com>
2024-07-12 13:57:29 +0800

    修改了各个图形的坐标点为左下角，增加了文件保存功能

--- server_main.py
+++ server_main.py
@@ -1,34 +1,30 @@
 from PySide6 import QtWidgets, QtGui, QtCore
 from PySide6.QtCore import Qt, QPointF, QTimer
 from PySide6.QtGui import QTransform, QAction, QIcon, QPainter, QLinearGradient, QColor
-from PySide6.QtWidgets import QWidget, QSizePolicy, QProgressBar, QProgressDialog, QVBoxLayout, QLabel, QDialog
+from PySide6.QtWidgets import QWidget, QSizePolicy, QProgressBar, QProgressDialog, QVBoxLayout, QLabel, QDialog, \
+    QInputDialog, QMessageBox, QFileDialog
 from qt_material import apply_stylesheet
 # import qtawesome as qta
 import qdarkstyle
 import json
 import math
 import sys
+import os
 
 from connector_server import start_server_thread
 from share import ShareInfo
+import share
 
 canvas_X_MAX = 958
 canvas_Y_MAX = 600
 # X_MAX = 210， Y_MAX = 150，机械臂活动范围
 shape_draw = ["rectangle", "ellipse", "line", "diamond", "pentagon", "hexagon"]
-tableStyle = '''
-QTableWidget {
-    gridline-color: #e0e0e0;
-}
+point_format = '{:.1f}'
+qss_file = './QSS_files/Ubuntu.qss'
+end = '_w.png'
 
-QHeaderView::section {     
-    background-color: #f8f8f8;
-    border-top: 0px solid #e0e0e0;
-    border-left: 0px solid #e0e0e0;
-    border-right: 1px solid #e0e0e0;
-    border-bottom: 1px solid #e0e0e0;
-    }
-'''
+
+# end = '.png'  # 文件后缀名，_w 指white
 
 
 class ProgressBar(QDialog):
@@ -49,6 +45,11 @@ class ProgressBar(QDialog):
         # layout.addWidget(self.text)
         layout.addWidget(self.text, 0, Qt.AlignCenter)
         # self.setLayout(layout)
+
+    def closeEvent(self, event):
+        ShareInfo.gstore.clear_buff()
+
+        event.accept()
 
 
 class Item:
@@ -92,12 +93,16 @@ class DiamondItem(Item, QtWidgets.QGraphicsPolygonItem):
     def __init__(self, *args):
         super().__init__(*args)
 
+        pen = self.pen()
+        pen.setWidth(3)
+        self.setPen(pen)
+
         self.props = {
             '空间坐标': str(self.pos().x()) + '，' + str(self.pos().y()),
             # 'length': '10',
             '菱形边长': '100',
             '填充颜色': '222, 241, 255, 0',
-            '线条宽度': '1',
+            '线条宽度': '3',
             '线条颜色': '0, 0, 0',
             'zValue': '0.0',
         }
@@ -172,7 +177,7 @@ class DiamondItem(Item, QtWidgets.QGraphicsPolygonItem):
             return
 
     def remap_xy(self, x, y):
-        x = float(x)
+        x = float(x) + float(self.props['菱形边长']) / 1.414
         y = canvas_Y_MAX - float(y) - float(self.props['菱形边长']) * 1.414
         self.setPos(x, y)
 
@@ -202,12 +207,16 @@ class HexagonItem(Item, QtWidgets.QGraphicsPolygonItem):
     def __init__(self, *args):
         super().__init__(*args)
 
+        pen = self.pen()
+        pen.setWidth(3)
+        self.setPen(pen)
+
         self.props = {
             '空间坐标': '0，0',
             # 'length': '10',
             '六边形边长': '100',
             '填充颜色': '222, 241, 255, 0',
-            '线条宽度': '1',
+            '线条宽度': '3',
             '线条颜色': '0, 0, 0',
             'zValue': '0.0',
         }
@@ -311,12 +320,16 @@ class PentagonItem(Item, QtWidgets.QGraphicsPolygonItem):
     def __init__(self, *args):
         super().__init__(*args)
 
+        pen = self.pen()
+        pen.setWidth(3)
+        self.setPen(pen)
+
         self.props = {
             '空间坐标': str(self.pos().x()) + '，' + str(self.pos().y()),
             # 'length': '10',
             '五边形边长': '100',
             '填充颜色': '222, 241, 255, 0',
-            '线条宽度': '1',
+            '线条宽度': '3',
             '线条颜色': '0, 0, 0',
             'zValue': '0.0',
         }
@@ -414,7 +427,7 @@ class PentagonItem(Item, QtWidgets.QGraphicsPolygonItem):
         super().setPolygon(q_ls)
 
     def reamp_xy(self, x, y):
-        x = float(x)
+        x = float(x) + float(self.props['五边形边长']) * math.cos(math.radians(36))
         y = canvas_Y_MAX - float(y) - 0.5 * float(self.props['五边形边长']) / math.tan(math.radians(18))
         self.setPos(x, y)
 
@@ -423,12 +436,16 @@ class RectItem(Item, QtWidgets.QGraphicsRectItem):
     def __init__(self, *args):
         super().__init__(*args)
 
+        pen = self.pen()
+        pen.setWidth(3)
+        self.setPen(pen)
+
         self.props = {
             '空间坐标': '0，0',
             '矩形宽度': '100',
             '矩形高度': '100',
             '填充颜色': '222, 241, 255, 0',
-            '线条宽度': '1',
+            '线条宽度': '3',
             '线条颜色': '0, 0, 0',
             'zValue': '0.0',
         }
@@ -522,11 +539,15 @@ class EllipseItem(Item, QtWidgets.QGraphicsEllipseItem):
     def __init__(self, *args):
         super().__init__(*args)
 
+        pen = self.pen()
+        pen.setWidth(3)
+        self.setPen(pen)
+
         self.props = {
             '空间坐标': '0，0',
             '圆形直径': '100',
             '填充颜色': '222, 241, 255, 0',
-            '线条宽度': '1',
+            '线条宽度': '3',
             '线条颜色': '0, 0, 0',
             'zValue': '0.0',
         }
@@ -772,35 +793,51 @@ class DnDGraphicView(QtWidgets.QGraphicsView):
 
         if picName == shape_draw[0]:
             shape = RectItem(0, 0, 100, 100)
+            shape.setPos(e.position().x(), e.position().y() - 100)
+
         elif picName == shape_draw[1]:
             shape = EllipseItem(0, 0, 100, 100)
+            shape.setPos(e.position().x(), e.position().y() - 100)
+
         elif picName == shape_draw[2]:
             shape = LineItem(0, 0, 100, 0)
             pen = shape.pen()
             pen.setWidth(3)
             shape.setPen(pen)
-        elif picName == "text":
-            shape = TextItem("文本内容")
+            shape.setPos(e.position().x(), e.position().y())
+
         elif picName == shape_draw[3]:
             l = 100 / 1.414
             shape = DiamondItem([QPointF(0, 0), QPointF(l, l), QPointF(0, 2 * l), QPointF(-l, l)])
+            shape.setPos(e.position().x() + l, e.position().y() - 2 * l)
             # shape.props['length'] = str(shape.polygon().length())
-        elif picName == "hexagon":
+
+        elif picName == shape_draw[4]:
+            x_1 = 100 * math.cos(math.radians(36))
+            y_1 = 100 * math.sin(math.radians(36))
+            l = 50 / math.sin(math.radians(18))
+            x_2 = l * math.cos(math.radians(72))
+            y_2 = l * math.sin(math.radians(72))
+            shape = PentagonItem([QPointF(0, 0), QPointF(x_1, y_1), QPointF(x_2, y_2),
+                                  QPointF(-x_2, y_2), QPointF(-x_1, y_1)])
+            shape.setPos(e.position().x() + x_1, e.position().y() - y_2)
+
+        elif picName == shape_draw[5]:
             sq3 = 1.732
             shape = HexagonItem([QPointF(0, 0), QPointF(50, 50 * sq3), QPointF(150, 50 * sq3), QPointF(200, 0),
                                  QPointF(150, -50 * sq3), QPointF(50, -50 * sq3)])
-        elif picName == "pentagon":
-            x_2 = 100 * math.cos(math.radians(36))
-            y_2 = 100 * math.sin(math.radians(36))
-            l = 50 / math.sin(math.radians(18))
-            x_3 = l * math.cos(math.radians(72))
-            y_3 = l * math.sin(math.radians(72))
-            shape = PentagonItem([QPointF(0, 0), QPointF(x_2, y_2), QPointF(x_3, y_3),
-                                  QPointF(-x_3, y_3), QPointF(-x_2, y_2)])
+            shape.setPos(e.position().x(), e.position().y() - 50 * sq3)
 
-        shape.setPos(e.position())
+        elif picName == "text":
+            shape = TextItem("文本内容")
+        else:
+            print('no shape')
+            return
+
+        # shape.setPos(e.position())
         print(shape.pos().y(), self.rect().height())
         y = shape.pos().y()
+        x = shape.pos().x()
         if picName == "rectangle" or picName == "ellipse":
             y = canvas_Y_MAX - shape.pos().y() - shape.rect().height()
         elif picName == "line":
@@ -811,7 +848,7 @@ class DnDGraphicView(QtWidgets.QGraphicsView):
             y = canvas_Y_MAX - float(y) - 100 * 1.732 / 2
         elif picName == "pentagon":
             y = canvas_Y_MAX - float(y) - 0.5 * 100 / math.sin(math.radians(18))
-        shape.props['空间坐标'] = str(shape.pos().x()) + '，' + '{:.1f}'.format(y)
+        shape.props['空间坐标'] = str('{:.1f}'.format(x) + '，' + '{:.1f}'.format(y))
         # print(shape.pos())
         self.scene().addItem(shape)
 
@@ -845,7 +882,8 @@ class MWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.resize(1400, 900 - 240)  # toolbar.height = 30
+        self.cwd = os.getcwd()
+        self.resize(1400, 900 - 242)  # toolbar.height = 30
 
         # central Widget
         centralWidget = QtWidgets.QWidget(self)
@@ -905,6 +943,15 @@ class MWindow(QtWidgets.QMainWindow):
         actionZeroItem = toolbar.addAction("置零")
         actionZeroItem.triggered.connect(self.zero)
 
+        actionZeroItem = toolbar.addAction("设置")
+        actionZeroItem.triggered.connect(self.set_fz)
+
+        actionZeroItem = toolbar.addAction("save")
+        actionZeroItem.triggered.connect(self.save_t)
+
+        actionZeroItem = toolbar.addAction("open")
+        actionZeroItem.triggered.connect(self.open_t)
+
         # 添加右侧的伸缩空间，将后续的 Action 推到最右边
         # spacer = QWidget()
         # spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -917,19 +964,68 @@ class MWindow(QtWidgets.QMainWindow):
         # action.triggered.connect(self.start)
         # print(toolbar.height())
 
-    def paintEvent(self, event):
-        # 创建QPainter对象
-        painter = QPainter(self)
+    # def paintEvent(self, event):
+    #     # 创建QPainter对象
+    #     painter = QPainter(self)
+    #
+    #     # 创建一个线性渐变对象
+    #     # 这里设置渐变从左上角到右下角，颜色从蓝色到红色
+    #     gradient = QLinearGradient(self.rect().topLeft(), self.rect().bottomRight())
+    #     gradient.setColorAt(0, QColor(0, 0, 255))  # 蓝色
+    #     gradient.setColorAt(1, QColor(255, 0, 0))  # 红色
+    #
+    #     # 使用渐变填充背景
+    #     painter.setBrush(gradient)
+    #     painter.drawRect(self.rect())
 
-        # 创建一个线性渐变对象
-        # 这里设置渐变从左上角到右下角，颜色从蓝色到红色
-        gradient = QLinearGradient(self.rect().topLeft(), self.rect().bottomRight())
-        gradient.setColorAt(0, QColor(0, 0, 255))  # 蓝色
-        gradient.setColorAt(1, QColor(255, 0, 0))  # 红色
+    def open_t(self):
+        fileName_read = QFileDialog.getOpenFileName(self,
+                                                    "文件读取",
+                                                    self.cwd,
+                                                    "json (*.json)")
+        if str(fileName_read[0]) == "":
+            QMessageBox.information(self, "提示", "未能成功读取文件，请重新读取。")  # 调用弹窗提示
+        else:
+            print(fileName_read[0])
+            with open(fileName_read[0], 'r', encoding='utf8') as f:
+                content = f.read()
 
-        # 使用渐变填充背景
-        painter.setBrush(gradient)
-        painter.drawRect(self.rect())
+            data: list = json.loads(content)
+            data.reverse()
+
+            for itemData in data:
+                typeName = itemData["type"]
+
+                theClass = globals()[typeName]
+                item = theClass()
+                item.loadData(itemData)
+                self.scene.addItem(item)
+
+                # 设置item可以移动
+                # item.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
+                # 设置item可以选中
+                item.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable, True)
+                # 设置item可以聚焦，这样才会有键盘按键回调keyPressEvent
+                item.setFlag(QtWidgets.QGraphicsItem.ItemIsFocusable, True)
+
+    def save_t(self):
+        fileName_save = QFileDialog.getSaveFileName(self,
+                                                    "文件保存",
+                                                    self.cwd,  # 起始路径
+                                                    "json file (*.json)")  # 设置文件扩展名过滤,用双分号间隔)
+        if str(fileName_save[0]) == "":
+            QMessageBox.information(self, "提示", "没有保存数据,请重新保存。")  # 调用弹窗提示
+        else:
+            itemSaveDataList = []
+            for item in self.scene.items():
+                if hasattr(item, 'toSaveData'):
+                    # print(item.pos().x(), item.pos().y())
+                    saveData = item.toSaveData()
+                    itemSaveDataList.append(saveData)
+
+            content = json.dumps(itemSaveDataList, indent=4, ensure_ascii=False)
+            with open(fileName_save[0], 'w', encoding='utf8') as f:
+                f.write(content)
 
     def load(self):
         with open('data/cfg.json', 'r', encoding='utf8') as f:
@@ -966,6 +1062,13 @@ class MWindow(QtWidgets.QMainWindow):
         content = json.dumps(itemSaveDataList, indent=4, ensure_ascii=False)
         with open('data/cfg.json', 'w', encoding='utf8') as f:
             f.write(content)
+
+    def set_fz(self):
+        d, ok = QInputDialog.getDouble(self, "设置机械臂Z轴高度", "请输入Z轴的值:",
+                                       share.f_z, -10000, maxValue=10000, decimals=2,
+                                       flags=Qt.WindowFlags(), step=1)
+        share.f_z = d
+        print(share.f_z)
 
     def get_pos(self):
         for item in self.scene.selectedItems():
@@ -1072,7 +1175,7 @@ class MWindow(QtWidgets.QMainWindow):
         row, col = 0, 0
 
         for shape in shape_draw:
-            file_path = f'./images/{shape}_w.png'
+            file_path = f'./images/{shape}{end}'
             pixmap = QtGui.QPixmap(file_path)
             # 设定图片缩放大小，这里是40个像素的宽度，高度也会等比例缩放
             pixmap = pixmap.scaledToWidth(40, Qt.SmoothTransformation)
@@ -1093,7 +1196,7 @@ class MWindow(QtWidgets.QMainWindow):
                 col += 1
 
     def setupCanvas(self):
-        self.scene = QtWidgets.QGraphicsScene(0, 0, canvas_X_MAX - 3, canvas_Y_MAX)
+        self.scene = QtWidgets.QGraphicsScene(0, 0, canvas_X_MAX - 5, canvas_Y_MAX)
         self.scene.addItem(QtWidgets.QGraphicsRectItem(0, 0, 936, canvas_Y_MAX))
         self.view = DnDGraphicView(self.scene)
         # self.view.centerOn(QPointF(-50, -50))
@@ -1106,7 +1209,6 @@ class MWindow(QtWidgets.QMainWindow):
         # 绘制属性面板
         self.tabWidget = QtWidgets.QTableWidget(0, 2, self)
         self.tabWidget.verticalHeader().hide()  # 隐藏垂直表头
-        # self.tabWidget.setStyleSheet(tableStyle)
         self.tabWidget.setHorizontalHeaderLabels(['属性', '值'])
         self.tabWidget.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
         self.tabWidget.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
@@ -1122,7 +1224,7 @@ class MWindow(QtWidgets.QMainWindow):
         layout_H_1.addLayout(layout_V_3)
 
         button_start = QtWidgets.QPushButton('启动绘图', self)
-        button_clear_buff = QtWidgets.QPushButton('清空缓冲', self)
+        button_clear_buff = QtWidgets.QPushButton('暂定..', self)
         button_draws = QtWidgets.QPushButton('删除选中', self)
         button_clear_scene = QtWidgets.QPushButton('清空画布', self)
         # button_start.setStyleSheet()
@@ -1136,7 +1238,7 @@ class MWindow(QtWidgets.QMainWindow):
         #                         "QPushButton:hover{background-color: rgb(22,218,208);}"
         #                         "QPushButton:pressed{background-color: rgb(17,171,164);}")
         button_draws.clicked.connect(self.delItem)
-        button_clear_buff.clicked.connect(self.clear_buff)
+        # button_clear_buff.clicked.connect(self.clear_buff)
         button_clear_scene.clicked.connect(self.delAllItems)
         button_start.clicked.connect(self.start)
 
@@ -1205,11 +1307,10 @@ if __name__ == '__main__':
     window = MWindow()
 
     # 第一种设置主题方法，通过 qss 文件
-    # qss_file = './QSS_files/dark.qss'
     # window.setStyleSheet(load_qss(qss_file))
 
     # 第二种设置主题方法，通过 apply_stylesheet
-    # apply_stylesheet(app, theme='dark_blue.xml')
+    # apply_stylesheet(app, theme=‘dark_cyan.xml’)
 
     # 第三种设置主题方法，通过 qdarkstyle
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyside6())
@@ -1217,7 +1318,7 @@ if __name__ == '__main__':
     pa.ID = 'dark'  # 调色板，可选 'dark', 'light'
     app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyside6', palette=pa))
 
-    window.scene.setBackgroundBrush(QColor(255,255,255))    # 设置画布背景色为白色
+    window.scene.setBackgroundBrush(QColor(255, 255, 255))  # 设置画布背景色为白色
     window.show()
     window.setWindowIcon(QtGui.QPixmap('./images/logo.png'))
     window.setWindowTitle('绘图窗口')
